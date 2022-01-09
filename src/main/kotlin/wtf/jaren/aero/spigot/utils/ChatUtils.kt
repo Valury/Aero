a15a5834eb792e481abaea489dce66e9a67c4f86
Jaren Knodel <Jaren@Knodel.com>
2022-01-09 09:44:58 -0500

    Improve downsample function

--- src/main/kotlin/wtf/jaren/aero/spigot/utils/ChatUtils.kt
+++ src/main/kotlin/wtf/jaren/aero/spigot/utils/ChatUtils.kt
@@ -2,12 +2,32 @@ package wtf.jaren.aero.spigot.utils
 
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.TextComponent
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextColor
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import java.util.regex.Pattern
 
 object ChatUtils {
+    val colorsToLegacy = mapOf(
+        NamedTextColor.BLACK to '0',
+        NamedTextColor.DARK_BLUE to '1',
+        NamedTextColor.DARK_GREEN to '2',
+        NamedTextColor.DARK_AQUA to '3',
+        NamedTextColor.DARK_RED to '4',
+        NamedTextColor.DARK_PURPLE to '5',
+        NamedTextColor.GOLD to '6',
+        NamedTextColor.GRAY to '7',
+        NamedTextColor.DARK_GRAY to '8',
+        NamedTextColor.BLUE to '9',
+        NamedTextColor.GREEN to 'a',
+        NamedTextColor.AQUA to 'b',
+        NamedTextColor.RED to 'c',
+        NamedTextColor.LIGHT_PURPLE to 'd',
+        NamedTextColor.YELLOW to 'e',
+        NamedTextColor.WHITE to 'f',
+    )
     fun getMessageAsComponent(source: CommandSender, viewer: Audience, text: String): TextComponent {
         var newText = text
         val builder = LegacyComponentSerializer.builder()
@@ -42,6 +62,7 @@ object ChatUtils {
     }
 
     private val COLOR_PATTERN: Pattern = Pattern.compile("§([0-9a-f]|#[0-9a-f]{6})")
+    private val HEX_PATTERN: Pattern = Pattern.compile("§#[0-9a-f]{6}")
     fun convertUnicodePrefixToColoredText(text: String): String {
         val prefix = convertUnicodeToPlainText(text)
         if (!prefix.startsWith("§f")) {
@@ -58,9 +79,14 @@ object ChatUtils {
     }
 
     fun downsampleHexColors(string: String): String {
-        return LegacyComponentSerializer.legacySection().serialize(
-            LegacyComponentSerializer.builder().hexColors().build().deserialize(string)
-        ).replace("§1", "§9")
+        var newString = string
+        val matcher = HEX_PATTERN.matcher(string)
+        while (matcher.find()) {
+            val code = matcher.group()
+            val color = TextColor.color(code.substring(2).toInt(16))
+            newString = newString.replace(code, "§${colorsToLegacy[NamedTextColor.nearestTo(color)]}")
+        }
+        return newString
     }
 
     fun downsampleHexColorsAmpersand(string: String): String {
